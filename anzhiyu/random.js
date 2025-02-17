14114c55fa84318d90db4d9ffbb45a0693d5ca15
lyay23 <3050681193@qq.com>
2025-02-17 17:08:04 +0800

    Site updated: 2025-02-17 17:08:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/66e64d45/","posts/9ec06885/","posts/6ead9846/","posts/28799/","posts/79666db/","posts/e255a10a/","posts/fc2c180e/","posts/e2e289e8/"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
+var posts=["posts/66e64d45/","posts/9ec06885/","posts/6ead9846/","posts/28799/","posts/79666db/","posts/e255a10a/","posts/fc2c180e/","posts/e2e289e8/","posts/12826/"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
